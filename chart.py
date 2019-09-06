0175683636e6e7ed89b56b4a5b7ed0914c9c5363
Unknown <joey.abouharb@gmail.com>
2019-09-06 16:54:30 +1000

    added titles to screen, need to figure out min and max price next commit

--- chart.py
+++ chart.py
@@ -10,7 +10,10 @@ from config import (
     WHITE, BLACK, UP_CANDLE_COLOUR,
     AVG_LINE_COLOUR, SCREEN_WIDTH, SCREEN_HEIGHT
 )
-
+from candlestick import (
+    draw_candlestick
+)
+from axes import draw_x_axis, draw_y_axis
 SAMPLE_DATA = '{"ticker_symbol": "EXPL", "price_history": [{"open": 14.0,"close": 14.1,"low": 15.0,"high": 14.0,"date": ""}]}'
 
 def draw_candlestick_chart(screen, chart_data, drawing_rect):
@@ -21,11 +24,14 @@ def draw_candlestick_chart(screen, chart_data, drawing_rect):
        * drawing_rect is (x, y, w, h)
     """
     data = json.loads(SAMPLE_DATA)
-    print(SAMPLE_DATA)
+    print(data)
     pygame.init()
+    high_line = ((data["price_history"][0]['high'], 120), (data["price_history"][0]['close'], 100))
+    stock_data = (80, 150 + data["price_history"][0]['close'], 40, data["price_history"][0]['open'] - data["price_history"][0]['close'])
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    # my_font = pygame.font.SysFont(FONT_NAME, FONT_PX)
+    my_font = pygame.font.SysFont('Noto Sans', 18)
     running = True
     while running:
         for event in pygame.event.get():
@@ -33,7 +39,21 @@ def draw_candlestick_chart(screen, chart_data, drawing_rect):
                 running = False
                 pygame.quit()
         screen.fill(WHITE)
-        pygame.draw.rect(screen, BLACK, (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), 2)
+        pygame.draw.rect(screen, BLACK, (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), 5)
+        pygame.draw.rect(screen, BLACK, stock_data)
+
+        #draw_candlestick(screen, False, stock_data)
+        title = my_font.render(
+            f'Stock Prices for {data["ticker_symbol"]}', True, BLACK)
+        screen.blit(title, (430, 50))
+        y_subtitle = my_font.render(
+            f'Min and Max Price ($)', True, BLACK)
+        y_subtitle = pygame.transform.rotate(y_subtitle, 90)
+        x_subtitle = my_font.render(
+            'Date Range - Daily', True, BLACK
+        )
+        screen.blit(x_subtitle, (430, 680))
+        screen.blit(y_subtitle, (40, 300))
         pygame.display.flip()
 
 draw_candlestick_chart(None, None, None)
