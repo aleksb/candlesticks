5e0f9a37a762a569de5ace2c4ab7759df7e81311
Samuel Allen <samuelcallen@me.com>
2019-09-07 09:28:48 +1000

    initial commit for API branch

--- data_source.py
+++ data_source.py
@@ -1,11 +1,31 @@
-
+#!/usr/bin/env python3
+import requests
 
 def import_stock_prices(company, start_date, end_date):
     """Imports a range of stock prices from an API.
 
     
-       * company - string company name (must be uppercase)
-       * start_date - datetime.date object
-       * end_date - datetime.date object
-       Returns stock data formatted as described in README.md.
-       """
+        * company - string company name (must be uppercase)
+        * start_date - e.g year-date-month
+        * end_date -  e.g year-date-month
+        Returns stock data formatted as described in README.md.
+        """
+
+    URL = 'https://financialmodelingprep.com/api/v3/historical-price-full/'
+    stock_data = requests.get(URL + company + "?from=" + start_date + '-01' + "&to=" + end_date + '-31')
+    return stock_data.json()
+
+dump = import_stock_prices('AAPL', '2018-03', '2018-04')
+count = 0
+lowsum = []
+
+#testing averaging lows
+for data in dump:
+    for key in data:
+        try:
+            lowsum.append(dump['historical'][count]['low'])
+            count += 1
+            print(sum(lowsum) / count)
+        except:
+            break
+
