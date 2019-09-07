2ebbcba668b7839abd69d71707d46f5810b44f95
Samuel Allen <samuelcallen@me.com>
2019-09-07 15:35:44 +1000

    moved parsing functionality into main function body, removed extra info and stored to new list of dictionaries

--- data_source.py
+++ data_source.py
@@ -12,34 +12,41 @@ def import_stock_prices(company, start_date, end_date):
         """
 
     URL = 'https://financialmodelingprep.com/api/v3/historical-price-full/'
-    stock_data = requests.get(URL + company + "?from=" + start_date + '-01' + "&to=" + end_date + '-31')
-    return stock_data.json()
+    stock_data = requests.get(URL + company + "?from=" + start_date + '-01&to=' + end_date + '-31')
+    #return stock_data.json()
+    dump = stock_data.json()
 
-dump = import_stock_prices('AAPL', '2018-03', '2018-04')
-count = 0
-lowsum = []
+    count = 0
 
-#testing averaging lows
-for data in dump:
-    for key in data:
-        try:
-            lowsum.append(dump['historical'][count]['low'])
-            count += 1
-            final_dict = {
-                'ticker_symbol': dump['symbol'],
-                'price_history': [
-                    {
-                        'open': dump['historical'][count]['open'],
-                        'close': dump['historical'][count]['close'],
-                        'low': dump['historical'][count]['low'],
-                        'high': dump['historical'][count]['high'],
-                        'date': dump['historical'][count]['label'],
-                    }
-                ]
-            }
+    lowsum = []
+    #lowmin = 0
 
-            print(final_dict)
-            # print(sum(lowsum) / count)
-        except:
-            break
+    highsum = []
+    #highmax = 0
 
+    #stock_average = 0
+    stock_list = []
+
+    for data in (dump['historical']):
+        print(data['low'])
+        lowsum.append(data['low'])
+        highsum.append(data['high'])
+        count += 1
+        final_dict = {
+            'ticker_symbol': dump['symbol'],
+            'price_history': [
+                {
+                    'open': data['open'],
+                    'close': data['close'],
+                    'low': data['low'],
+                    'high': data['high'],
+                    'date': data['label'],
+                }
+            ]
+        }
+        stock_list.append(final_dict)
+    return stock_list
+
+
+stock_list = (import_stock_prices('AAPL', '2019-03', '2019-05'))
+print(stock_list)
