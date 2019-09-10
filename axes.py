6f18fcee4bbaf2c7a4b8368e1d09158f1866cbf4
joeyabouharb <joeyabouharb@gmail.com>
2019-09-10 10:17:19 +1000

    refactored code, candlestick center created x axis points for each date

--- axes.py
+++ axes.py
@@ -1,5 +1,5 @@
-
-
+import pygame
+from config import BLACK
 def draw_x_axis(screen, x, y, w, start_date, end_date):
        """Draws the x axis
        
@@ -27,16 +27,15 @@ def draw_y_axis(screen, x, y, h, min_price, max_price):
        * min_price - in USD
        * max_price - in USD
        """
-       pygame.draw.lines(screen, BLACK , False, [(x,y),(x,h)], 2)
-       text_snippet_min = my_font.render(f"{min_price}", True, BLACK)
-       screen.blit(text_snippet_min, ((x-50), (y+h)))
-       text_snippet_max = my_font.render(f"{max_price}", True, BLACK)
-       screen.blit(text_snippet_max, ((x-50), (y+5)))
+       pygame.draw.aaline(screen, BLACK, x, y, 10)
+       # text_snippet_min = my_font.render(f"{min_price}", True, BLACK)
+       # screen.blit(text_snippet_min, ((x-50), (y+h)))
+       # text_snippet_max = my_font.render(f"{max_price}", True, BLACK)
+       # screen.blit(text_snippet_max, ((x-50), (y+5)))
        
        spread = max_price - min_price
        
-       text_snippet_mid = my_font.render(f"{max_price-spread/2}", True, BLACK)
+       # text_snippet_mid = my_font.render(f"{max_price-spread/2}", True, BLACK)
        # screen.blit(text_snippet_mid,())
-       
-              
+
 
--- chart.py
+++ chart.py
@@ -5,6 +5,9 @@ Draws a candlestick chart on a PyGame screen.
 import json
 import pygame
 import datetime
+from operator import (
+    itemgetter
+)
 from pygame.locals import QUIT
 from config import (
     WHITE, BLACK, UP_CANDLE_COLOUR,
@@ -14,7 +17,8 @@ from candlestick import (
     draw_candlestick
 )
 from axes import draw_x_axis, draw_y_axis
-SAMPLE_DATA = '{"symbol": "EXPL", "historical": [{"open": 14.0,"close": 14.1,"low": 15.0,"high": 14.0,"date": ""}]}'
+
+
 
 def draw_candlestick_chart(screen, chart_data, drawing_rect, my_font):
     """
@@ -23,30 +27,139 @@ def draw_candlestick_chart(screen, chart_data, drawing_rect, my_font):
        * chart_data formatted as described in README.md.
        * drawing_rect is (x, y, w, h)
     """
-    high_line = ((chart_data["historical"][0]['high'], 120), (chart_data["historical"][0]['close'], 100))
-    candlestick_points = (
-        80, 150 + chart_data["historical"][0]['close'], 40,
-        chart_data["historical"][0]['open'] - chart_data["historical"][0]['close']
+    graph = pygame.draw.rect(screen, BLACK, drawing_rect, 5)
+
+    x_xaxis, y_xaxis = get_coordinates(graph, "x_axis")
+
+    x_axis = draw_x_axis_joe(
+        screen,
+        x_xaxis, y_xaxis,
     )
-    screen.fill(WHITE)
-    pygame.draw.rect(screen, BLACK, drawing_rect, 5)
-    pygame.draw.rect(screen, BLACK, candlestick_points)
+    x_yaxis, y_yaxis = get_coordinates(graph, "y_axis")
+
+    y_axis = draw_y_axis_joe(
+        screen,
+        x_yaxis, y_yaxis
+    )
+
+    draw_title(
+        screen,
+        my_font,
+        chart_data["symbol"],
+        graph
+    )
+
+    draw_subtitles(
+        screen,
+        my_font,
+        x_axis,
+        graph,
+        y_axis
+    )
     # draw_candlestick(screen, False, stock_data)
+    xlabels = draw_x_labels(
+        screen,
+        x_axis,
+        chart_data['historical']
+    )
+    descending_order = sorted(
+        chart_data['historical'],
+        key=itemgetter('high'),
+        reverse=True
+    )
+    print(descending_order[0]['high'])
+    for i, label in enumerate(xlabels):
+        pass # pass this data and the historical to the candles
+    pygame.display.flip()
+
+
+def get_coordinates(graph, cooridinates):
+    return {
+        "x_axis": ((
+            graph.left + 100,
+            graph.bottom - 100
+        ), (
+            graph.right - 100,
+            graph.bottom - 100
+        )),
+        "y_axis": ((
+            graph.left + 100,
+            graph.bottom - 100
+        ), (
+            graph.left + 100,
+            graph.top + 100
+        ))
+    }.get(cooridinates)
+
+
+# other defs can go here
+def draw_x_axis_joe(screen, x, y):
+    """
+    draw x axis but return rect object. this will provide properties
+    like left, right. bottom, top and center
+    """
+    return pygame.draw.line(screen, BLACK, x, y)
+
+def draw_y_axis_joe(screen, x, y):
+    """
+    same thing, this will return a rect object with helpful props
+    """
+    return pygame.draw.line(screen, BLACK, x, y)
+
+def draw_title(screen, my_font, symbol, graph):
+    """
+    draw title
+    """
     title = my_font.render(
-        f'Stock Prices for {chart_data["symbol"]}', True, BLACK)
+    f'Stock Prices for {symbol}', True, BLACK)
+    title_left, title_right = graph.center[0] - 120, graph.top + 50
+    screen.blit(title, (title_left, title_right))
+
+def draw_subtitles(screen, my_font, x_axis, graph, y_axis):
+    """
+    draw subtitles using the x axis rect object. for a reference
+    """
     y_subtitle = my_font.render(
-        f'Min and Max Price ($)', True, BLACK)
+    f'Min and Max Price ($)', True, BLACK)
     y_subtitle = pygame.transform.rotate(y_subtitle, 90)
     x_subtitle = my_font.render(
         'Date Range - Daily', True, BLACK
     )
-    screen.blit(title, (430, 50))
-    screen.blit(x_subtitle, (430, 680))
-    screen.blit(y_subtitle, (40, 300))
-    pygame.display.flip()
 
-# other defs can go here
+    xsub_left, xsub_right = x_axis.center[0] - 80, graph.bottom - 80
+    ysub_left, ysub_right = graph.left + 25, y_axis.center[0] + 120
+    screen.blit(x_subtitle, (xsub_left, xsub_right))
+    screen.blit(y_subtitle, (ysub_left, ysub_right))
 
+def draw_x_labels(screen, x_axis, dates):
+    """
+    I want this to return
+    return a list of lines (Rects) this way i can easily determine candlestick center
+    """
+    labels = []
+    x_axis_length = x_axis.right - x_axis.left
+    stroke = int(x_axis_length / len(dates))
+    for value in range(
+        x_axis.left + stroke,
+        x_axis_length + stroke,
+        stroke
+    ):
+        labels.append(pygame.draw.line(
+            screen, BLACK,
+            (value, x_axis.bottom),
+            (value, x_axis.bottom + 5)
+        ))
+    return labels
+def draw_y_labels(screen, y_axis, data, highest_value):
+    """
+    draw y labels, these should be returned as rects as well
+    """
+    for element in data:
+        element['high']
+        element['low']
+
+# change the dict values as recieved from the api
+SAMPLE_DATA = '{"symbol": "EXPL", "historical": [{"open": 14.0,"close": 14.1,"low": 15.0,"high": 14.0,"date": ""}]}'
 
 if __name__ == "__main__":
     pygame.init()
@@ -59,6 +172,7 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
-
+        screen.fill(WHITE)
         draw_candlestick_chart(screen, data, (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), my_font)
     pygame.quit()
+
