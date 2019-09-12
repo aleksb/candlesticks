d66db167cb58022384b3902ab5d4219cd16e8786
Pau1Robinson <pr6784@gmail.com>
2019-09-12 14:20:10 +1000

    first pass at working candlestick

--- candlestick.py
+++ candlestick.py
@@ -1,4 +1,21 @@
 
+def calc_rect(screen, x, y, width, height, stock_info, close_high):
+    '''
+    calculates the values for the rect of the candlestick using screen, x, y, width, height and stock_info\n
+    uses the close_high bool to determine if the top the rect is close or the open and the color the rect should be.
+    '''
+    rect_x = x - (width / 2)
+    if close_high is True:
+        rect_y = (y + (((stock_info["high"] - stock_info["close"]) / (stock_info["high"] - stock_info["low"])) * h))
+        rect_h = (((stock_info["close"] - stock_info["open"]) / (stock_info["high"] - stock_info["low"])) * h)
+        rect_values = (screen, (0, 0, 0), (rect_x, rect_y, width, rect_h), 2)
+        return rect_values
+    elif close_high is False:
+        rect_y = (y + (((stock_info["high"] - stock_info["open"]) / (stock_info["high"] - stock_info["low"])) * h))
+        rect_h = (((stock_info["close"] - stock_info["open"]) / (stock_info["high"] - stock_info["low"])) * h)
+        rect_values = (screen, (0, 0, 0), (rect_x, rect_y, width, rect_h))
+        return rect_values
+
 def draw_candlestick(screen, x, y, h, cs_w, stock_info):
     """Draws a candlestick
     * screen
@@ -13,29 +30,12 @@ def draw_candlestick(screen, x, y, h, cs_w, stock_info):
             'high': usd,
         }
     """
-    #methodlogy - change stock_info dict in list then take the high and low as a perecentage of the height and width of the candlestick
-    rect_color = (255, 255, 255)
-    h = 400
-    x = 400
-    y = 200
-    cs_w = 50
-    stock_info = {"open":153.6368, "high":157.5648, "low":152.9822, "close":156.6423}
-    rect_x = x - (cs_w / 2)
-    if stock_info["open"] > stock_info["close"]:
-        rect_top = ""
+    pygame.draw.line(screen, (0, 0, 0), (x, y), (x, y + h), 2)
+    if stock_info["close"] > stock_info["open"]:
+        rect_values = calc_rect(screen, x, y, cs_w, h, stock_info, True)
+        pygame.draw.rect(rect_values[0], rect_values[1], rect_values[2], rect_values[3])
+        #Draws a white rect to cover the line inside the first rect
+        pygame.draw.rect(rect_values[0], (255, 255, 255), (rect_values[2][0] + 2, rect_values[2][1] + 2, rect_values[2][2] - 4, rect_values[2][3] - 2))
     else:
-        rect_y = (y + (((stock_info["high"] - stock_info["close"]) / (stock_info["high"] - stock_info["low"])) * h))
-        rect_h = (((stock_info["close"] - stock_info["open"]) / (stock_info["high"] - stock_info["low"])) * h)
-    rect_w = cs_w / 2
-    print(f"rect_x ={rect_x} rect_y = {rect_y} rect_h = {rect_h} rect_w = {rect_w}")
-    pygame.draw.rect(screen, (rect_color), (rect_x, rect_y, cs_w, rect_h), 2)
-    pygame.draw.line(screen, (rect_color), (x, y), (x, y + h), 2)
-
-    # list_cords = [stock_info[open], stock_info[close], stock_info[low], stock_info[high]]
-    # price_difference = list_cords[3] - list_cords[2] / h
-    # if list_cords[0] > list_cords[1]:
-    #    pass
-    # elif list_cords[0] < list_cords[1]:
-    #    pass
-    # elif list_cords[0] == list_cords[1]:
-    #     pass
+        rect_values = calc_rect(screen, x, y, cs_w, h, stock_info, False)
+        pygame.draw.rect(rect_values[0], rect_values[1], rect_values[2])
